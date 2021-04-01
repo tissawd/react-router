bd46a75a11b115711d48d85b2d35fbd3c45d4e8a
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-01 10:05:01 -0500

    added nav bar with links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="nav-bar">
+          <Link to="/home">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/view-cart">View Cart</Link>
+          <Link to="/checkout">Checkout</Link>
+        </nav>
       </header>
     </div>
   );
